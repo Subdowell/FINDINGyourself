3343e9fa171039b4899afabafe419c2449e73fb1
Subdowell <vit_bondar@mail.ru>
2020-09-09 17:20:40 -0400

    Add new model Vacancy

---
+++ scraping/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import City, Language, Vacancy
+
+admin.site.register(City)
+admin.site.register(Language)
+admin.site.register(Vacancy)
+
+# Register your models here.

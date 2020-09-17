0d9a8312de954ec0e13c4c03b9781603869e90b8
Subdowell <vit_bondar@mail.ru>
2020-09-17 11:53:44 -0400

    Add register form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from accounts.forms import UserLoginForm, UserRegistrationForm
+
+
+def login_views(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        data = form.cleaned_data
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts/register_done.html',
+                      {'new_user': new_user})
+    return render(request, 'accounts/register.html',
+                  {'form': form})
--- scraping_service/urls.py
+++ scraping_service/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from scraping.views import home_view, list_view
 
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('list/', list_view, name='list'),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
 ]

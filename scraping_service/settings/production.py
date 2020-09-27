6d0074594b2ca14a81141778c03f3bc3db6d8fff
Subdowell <vit_bondar@mail.ru>
2020-09-27 16:25:00 -0400

    Add settings in Heroku

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -38,7 +38,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://fin-you.herokuapp.com/"]
+ALLOWED_HOSTS = ["fin-you.herokuapp.com"]
 
 
 # Application definition
